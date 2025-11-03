cbacbb8626dfb7fd27a7debef81ff1432ab77425
loanrottier <loan.rottier@hotmail.fr>
2025-11-03 09:24:00 +0100

    Fusion de compustat et lopucki

--- test_Loan.R
+++ test_Loan.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(readxl)
 
 setwd("~/M2/Scoring/Projet")
 
@@ -6,6 +7,7 @@ variables <- readRDS("compustat_company_variables.rds")
 funda_db <- readRDS("compustat_funda_variables.rds")
 data <- readRDS("compustat_all_light.rds")
 compustat_all <- readRDS("compustat_all.rds")
+lopucki <- read_excel("Florida-UCLA-LoPucki Bankruptcy Research Database 1-12-2023.xlsx")
 
 na_vector <- colSums(is.na(data))
 variables$nz <- na_vector
@@ -25,3 +27,20 @@ data_2 <- compustat_all %>%
          SLTA = sale / at)
 ggplot(data_2, aes(fyear,WCTA))+
   geom_point(color = conm)
+
+### Lopucki ###
+lopucki_clean <- lopucki %>%
+  select(NameCorp, Chapter, GvkeyBefore, DateFiled) %>%
+  filter(Chapter %in% c('7', '11')) %>%
+  group_by(GvkeyBefore) %>%
+  summarize(DateFiled = min(DateFiled),
+            
+            NameCorp = NameCorp[which.min(DateFiled)],
+            Chapter = Chapter[which.min(DateFiled)]) %>%
+  mutate(DateFiled = lubridate::as_date(DateFiled)) %>%
+  ungroup()
+
+### Jointure Lopucki et Compustat
+df_joined <- left_join(compustat_all, lopucki_clean, by = c("gvkey" = "GvkeyBefore"))
+table(is.na(df_joined$DateFiled))
+
